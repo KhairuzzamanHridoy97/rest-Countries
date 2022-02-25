a3a4359310ddd7fab65b6fb76f3ca67f11ea183c
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-02-26 02:50:07 +0600

    added jsScript

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const loadCountries =()=>{
     fetch('https://restcountries.com/v2/all')
     .then(res=>res.json())
-    .then(data=>console.log(data))
+    .then(data=>displayCountries(data))
 }
+
+const displayCountries =  countries =>{
+    console.log(countries)
+}
+
+loadCountries();
