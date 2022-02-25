114bb270ba099e82669f74fd67afa6d3dd0630db
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-02-26 03:29:47 +0600

    use map

--- index.js
+++ index.js
@@ -5,7 +5,24 @@ const loadCountries =()=>{
 }
 
 const displayCountries =  countries =>{
-    console.log(countries)
+    console.log(countries[0]);
+    const container = document.getElementById('countries');
+    const countryHTML = countries.map(country => getCountyHTML(country));
+    container.innerHTML = countryHTML.join(' ')
+    console.log(countryHTML[0])
 }
+
+const getCountyHTML = country =>{
+    return `
+        <div class='country'>
+            <h2>${country.name} </h2>
+            <h4>${country.capital} </h4>
+            <img src="${country.flag}" />
+
+        </div>
+    `
+    
+}
+
 
 loadCountries();
